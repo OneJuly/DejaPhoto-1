1deb26816fb1eee2531ba104d54246ab19836371
Jean-Luc Thumm <jthumm@ucsd.edu>
2017-05-10 22:26:13 -0700

    Add widget boiler plate code and set up

---
+++ app/src/main/java/team4/cse110/dejaphoto/DejaAppWidgetProvider.java
@@ -0,0 +1,9 @@
+package team4.cse110.dejaphoto;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * AppWidgetProvider for the DejaVu class
+ */
+public class DejaAppWidgetProvider extends AppWidgetProvider {
+}
