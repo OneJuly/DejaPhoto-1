73c0b52bd6e5dc6e7cf7c77c733bdc1ee1ab72ec
yuw160 <yuw160@ucsd.edu>
2017-05-11 20:37:02 -0700

    began nextImage() and previousImage() functionality

--- app/src/main/java/team4/cse110/dejaphoto/ImageAlgorithms.java
+++ app/src/main/java/team4/cse110/dejaphoto/ImageAlgorithms.java
@@ -1,37 +1,87 @@
-package team4.cse110.dejaphoto;
-
 /**
  * Created by Alisa on 5/11/17.
  */
+
+package team4.cse110.dejaphoto;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.media.Image;
 
 public class ImageAlgorithms {
 
     ///////////////
     boolean DJV = true;
-    public void DJV_algorithm(){};
-    public void random_algorithm(){};
-    ///////////////
+    Image returnImage;
+    public Image DJV_algorithm(){
+        return returnImage;}
+    public Image random_algorithm(){
+        return returnImage;}
+    //////////////
 
-    //////
+    private Context context;
+    private int imageIndex;
+    Image[] previousImages = new Image[11];
 
-    private int picIndex;
-
-    public ImageAlgorithms(){
-
-        picIndex = 0;
-
+    public ImageAlgorithms(Context context){
+        this.context = context;
+        imageIndex = 0;
     }
 
-    public void nextImage(){
-        if (this.picIndex == 0){
+    //TODO get a DejaVuEnabled (or something) boolean out of sp for whether DejaVu Mode is enabled
+    SharedPreferences sp = context.getSharedPreferences("Settings",0);
+
+    //gets called for the next image
+    public Image nextImage(){
+        if(imageIndex == 0){
+            //TODO update DJV boolean
             if(DJV == true){
-
-
-
+                //TODO update DJV_algorithm
+                returnImage = DJV_algorithm();
+                //for indexes with images, copies images in array starting with [9]->[10]
+                for(int index = 9; index >-1; --index){
+                    if(previousImages[index] != null) {
+                        previousImages[index + 1] = previousImages[index];
+                    }
+                }
+                //updates image array and returns a new image
+                previousImages[0] = returnImage;
+                return returnImage;
             }
-
+            else{
+                //TODO update random_algorithm
+                returnImage = random_algorithm();
+                //for indexes with images, copies images in array starting with [9]->[10]
+                for(int index = 9; index > -1; --index){
+                    if(previousImages[index] != null){
+                        previousImages[index + 1] = previousImages[index];
+                    }
+                }
+                //updates image array and rturns a new image
+                previousImages[0] = returnImage;
+                return returnImage;
+            }
         }
-
+        //updates the imageIndex to point to the next image and returns that image
+        else{
+            imageIndex -= 1;
+            return previousImages[imageIndex];
+        }
     }
 
+    //gets called for the previous image
+    public Image previousImage(){
+        //goes back a maximum of 10 images
+        if(imageIndex == 10) {
+            return null;
+        }
+        //if there is no image to go back to
+        else if(previousImages[imageIndex+1] == null){
+            return null;
+        }
+        //returns the previous image (if there is one)
+        else{
+            imageIndex += 1;
+            return previousImages[imageIndex];
+        }
+    }
 }
