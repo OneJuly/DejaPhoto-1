e384b3c1c8a0edc6be5e60d8304c7046030108d2
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-05-11 23:51:33 -0700

    Add prev and next button with functionality

--- app/src/main/java/team4/cse110/dejaphoto/DejaAppWidgetProvider.java
+++ app/src/main/java/team4/cse110/dejaphoto/DejaAppWidgetProvider.java
@@ -15,14 +15,16 @@ import android.widget.RemoteViews;
 public class DejaAppWidgetProvider extends AppWidgetProvider {
 
     public static final String TAG = "DEJA_PROVIDER";
+    public static final String TAG_RECV = "Receive: ";
 
+    private static final String PREV_CLICKED = "PREV_BUTTON_CLICK";
     private static final String NEXT_CLICKED = "NEXT_BUTTON_CLICK";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         Log.v(TAG, "In update function");
 
-        // Get all associated ids
+        // Get all associated widget IDs
         ComponentName widget = new ComponentName(context, DejaAppWidgetProvider.class);
         int[] allIds = appWidgetManager.getAppWidgetIds(widget);
 
@@ -31,13 +33,21 @@ public class DejaAppWidgetProvider extends AppWidgetProvider {
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
                     R.layout.widget_deja);
 
-            // Register a pending intent
-            Intent intent = new Intent(context, DejaAppWidgetProvider.class);
-            intent.setAction(NEXT_CLICKED);
+            // Create intents for each button
+            Intent prevIntent = new Intent(context, DejaAppWidgetProvider.class);
+            prevIntent.setAction(PREV_CLICKED);
+            Intent nextIntent = new Intent(context, DejaAppWidgetProvider.class);
+            nextIntent.setAction(NEXT_CLICKED);
 
-            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent,
+            // Create pending intents
+            PendingIntent prevPendingIntent = PendingIntent.getBroadcast(context, 0, prevIntent,
                     PendingIntent.FLAG_UPDATE_CURRENT);
-            remoteViews.setOnClickPendingIntent(R.id.button_next, pendingIntent);
+            PendingIntent nextPendingIntent = PendingIntent.getBroadcast(context, 0, nextIntent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+
+            // Update remoteView then set to widget
+            remoteViews.setOnClickPendingIntent(R.id.button_prev, prevPendingIntent);
+            remoteViews.setOnClickPendingIntent(R.id.button_next, nextPendingIntent);
             appWidgetManager.updateAppWidget(id, remoteViews);
         }
     }
@@ -46,10 +56,12 @@ public class DejaAppWidgetProvider extends AppWidgetProvider {
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
 
-        Log.v(TAG, "Receive: " + intent.getAction());
+        Log.v(TAG, TAG_RECV + intent.getAction());
 
-        if (NEXT_CLICKED.equals(intent.getAction())) {
-            Log.v(TAG, "Receive: next clicked");
+        if (PREV_CLICKED.equals(intent.getAction())) {
+            Log.v(TAG, TAG_RECV + "Prev button tapped");
+        } else if (NEXT_CLICKED.equals(intent.getAction())) {
+            Log.v(TAG, TAG_RECV + "Next button tapped");
         }
     }
 }
