f40d8913227e18561c99152dbe7df8a2a14e2932
aprathna <a.prathnadi@gmail.com>
2017-05-11 19:20:35 -0700

    started

---
+++ app/src/main/java/team4/cse110/dejaphoto/ImageAlgorithms.java
@@ -0,0 +1,35 @@
+package team4.cse110.dejaphoto;
+
+/**
+ * Created by Alisa on 5/11/17.
+ */
+
+public class ImageAlgorithms {
+
+    ///////////////
+    boolean DJV = true;
+    public void DJV_algorithm(){};
+    public void random_algorithm(){};
+    ///////////////
+
+    private int picIndex;
+
+    public ImageAlgorithms(){
+
+        picIndex = 0;
+
+    }
+
+    public void nextImage(){
+        if (this.picIndex == 0){
+            if(DJV == true){
+
+
+
+            }
+
+        }
+
+    }
+
+}
