5b636ab698ca0fd571a300a5e873947169222d49
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-05-11 23:33:42 -0700

    Fix bug preventing onUpdate() from being called
    onReceive() must call superclass

--- app/src/main/java/team4/cse110/dejaphoto/DejaAppWidgetProvider.java
+++ app/src/main/java/team4/cse110/dejaphoto/DejaAppWidgetProvider.java
@@ -6,6 +6,7 @@ import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
@@ -13,46 +14,42 @@ import android.widget.RemoteViews;
  */
 public class DejaAppWidgetProvider extends AppWidgetProvider {
 
-    private static final String NEXT_CLICKED = "nextButtonClick";
+    public static final String TAG = "DEJA_PROVIDER";
+
+    private static final String NEXT_CLICKED = "NEXT_BUTTON_CLICK";
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // Inflate view for widget and set up identifier
-        RemoteViews remoteViews =
-                new RemoteViews(context.getPackageName(), R.layout.widget_deja);
-        ComponentName widget
-                = new ComponentName(context, DejaAppWidgetProvider.class);
+        Log.v(TAG, "In update function");
 
-        // Register pending intent for button
-        remoteViews.setOnClickPendingIntent
-                (R.id.button_next, getPendingSelfIntent(context, NEXT_CLICKED));
+        // Get all associated ids
+        ComponentName widget = new ComponentName(context, DejaAppWidgetProvider.class);
+        int[] allIds = appWidgetManager.getAppWidgetIds(widget);
 
-        // Update widget
-        appWidgetManager.updateAppWidget(widget, remoteViews);
+        for (int id : allIds) {
+            // Inflate the view hierarchy
+            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),
+                    R.layout.widget_deja);
+
+            // Register a pending intent
+            Intent intent = new Intent(context, DejaAppWidgetProvider.class);
+            intent.setAction(NEXT_CLICKED);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+            remoteViews.setOnClickPendingIntent(R.id.button_next, pendingIntent);
+            appWidgetManager.updateAppWidget(id, remoteViews);
+        }
     }
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        Log.v(TAG, "Receive: " + intent.getAction());
+
         if (NEXT_CLICKED.equals(intent.getAction())) {
-            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
-
-            // Inflate view for widget and set up identifier
-            RemoteViews remoteViews =
-                    new RemoteViews(context.getPackageName(), R.layout.widget_deja);
-            ComponentName widget
-                    = new ComponentName(context, DejaAppWidgetProvider.class);
-
-            // Change text of button
-            remoteViews.setTextViewText(R.id.button_next, "CLICKED");
-
-            // Update widget
-            appWidgetManager.updateAppWidget(widget, remoteViews);
+            Log.v(TAG, "Receive: next clicked");
         }
-    }
-
-    protected PendingIntent getPendingSelfIntent(Context context, String action) {
-        Intent intent = new Intent(context, getClass()); // new intent to this class
-        intent.setAction(action);
-        return PendingIntent.getBroadcast(context, 0, intent, 0);
     }
 }
