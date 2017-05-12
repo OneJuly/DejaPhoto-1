879dec72be3ddedab5eb9e1d2715ddd94a56a460
seanssel <sselvig@ucsd.edu>
2017-05-12 11:39:28 -0700

    Add new .gitignore

--- app/src/main/java/team4/cse110/dejaphoto/ImageAdapter.java
+++ app/src/main/java/team4/cse110/dejaphoto/ImageAdapter.java
@@ -4,6 +4,10 @@ import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 
 /**
@@ -20,7 +24,7 @@ class ImageAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return thumbsID.length;
     }
 
     @Override
@@ -35,6 +39,51 @@ class ImageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(250, 250));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setPadding(4, 4, 4, 4);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        Glide
+                .with(context)
+                .load(thumbsID[position])
+                .into(imageView);
+
+        return imageView;
     }
+
+    private Integer[] thumbsID = {
+            R.drawable.sample_0, R.drawable.sample_2,
+            R.drawable.sample_3, R.drawable.sample_4,
+            R.drawable.sample_1, R.drawable.sample_4,
+            R.drawable.sample_5, R.drawable.sample_0,
+            R.drawable.sample_1, R.drawable.sample_3,
+            R.drawable.sample_3, R.drawable.sample_4,
+            R.drawable.sample_1, R.drawable.sample_4,
+            R.drawable.sample_5, R.drawable.sample_0,
+            R.drawable.sample_1, R.drawable.sample_3,
+            R.drawable.sample_6, R.drawable.sample_2,
+            R.drawable.sample_6, R.drawable.sample_2,
+            R.drawable.sample_0, R.drawable.sample_2,
+            R.drawable.sample_3, R.drawable.sample_4,
+            R.drawable.sample_1, R.drawable.sample_4,
+            R.drawable.sample_5, R.drawable.sample_0,
+            R.drawable.sample_1, R.drawable.sample_3,
+            R.drawable.sample_6, R.drawable.sample_2,
+            R.drawable.sample_1, R.drawable.sample_4,
+            R.drawable.sample_1, R.drawable.sample_4,
+            R.drawable.sample_3, R.drawable.sample_4,
+            R.drawable.sample_1, R.drawable.sample_4,
+            R.drawable.sample_5, R.drawable.sample_0,
+            R.drawable.sample_1, R.drawable.sample_3,
+            R.drawable.sample_6, R.drawable.sample_2,
+            R.drawable.sample_1, R.drawable.sample_4,
+            R.drawable.sample_1, R.drawable.sample_4,
+            R.drawable.sample_1, R.drawable.sample_4,
+    };
 }
