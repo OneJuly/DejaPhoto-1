17de5b989916be7ac6a45440367d0991d820a290
seanssel <sselvig@ucsd.edu>
2017-05-12 17:10:14 -0700

    Impleent onCreateViewHolder

--- app/src/main/java/team4/cse110/dejaphoto/ImageAdapter.java
+++ app/src/main/java/team4/cse110/dejaphoto/ImageAdapter.java
@@ -2,15 +2,21 @@ package team4.cse110.dejaphoto;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import java.util.List;
 
 /**
  * This is the adapter for the RecyclerView thumbnails in GalleryActivity.
  */
 
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> {
+
+    public ImageAdapter() {
+    }
 
     /* Internal photo viewholder */
     public static class ViewHolder extends RecyclerView.ViewHolder {
@@ -27,9 +33,26 @@ public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ViewHolder> 
     private Context context;
     private List<Photo> photos;
 
+    public ImageAdapter(Context context, List<Photo> photos) {
+        this.context = context;
+        this.photos = photos;
+    }
+
+    private Context getContext() {
+        return context;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        Context c = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(c);
+
+        // Inflate the gallery_photo layout
+        View photoView = inflater.inflate(R.layout.gallery_photo, parent, false);
+
+        // Return the new viewholder
+        ViewHolder holder = new ViewHolder(photoView);
+        return holder;
     }
 
     @Override
--- app/src/main/java/team4/cse110/dejaphoto/Photo.java
+++ app/src/main/java/team4/cse110/dejaphoto/Photo.java
@@ -1,83 +1,34 @@
 package team4.cse110.dejaphoto;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.media.ExifInterface;
-
-import java.util.Date;
+import android.net.Uri;
 
 /**
  * Created by Sam Wang on 5/11/2017.
  */
 
-//https://github.com/drewnoakes/metadata-extractor
-//http://stackoverflow.com/questions/28502206/format-time-and-date-which-is-get-from-exifinterface-tag-datetime
-//http://stackoverflow.com/questions/5175728/how-to-get-the-current-date-time-in-java
-
 public class Photo {
 
-    //Metadata metadata = ImageMetadataReader.readMetadata(jpegFile);
-    //Metadata metadata = ImageMetadataReader.readMetadata(stream);
-
-    //TODO replace member variables with their appropriate types
-    Bitmap returnImage;
-    int dayTime;
-    String weekday;
-    int location;
-    boolean karma;
-    //////////
-
     private Context context;
-    int weight;
-    ExifInterface exifInterface;
-    String weekdayFormat;
-    String timeFormat;
-    Date d;
-    String time;
+    private Uri uri;
 
     //constructor for the photo class
-    public Photo(Context context){
+    public Photo(Context context, Uri uri){
         this.context = context;
-        dayTime = 0;
-        weekday = "";
-        location = 0;
-        karma = false;
-        weight = 0;
+        this.uri = uri;
+    }
+
+    public Uri getUri() {
+        return uri;
     }
 
     public String getWeekday() {
         return "";
     }
-//        weekday = exifInterface.getAttribute(ExifInterface.TAG_DATETIME);
-//        SimpleDateFormat weekdayF = new SimpleDateFormat("EEE");
-//        try {
-//            d = weekdayF.parse(weekday);
-//        } catch (ParseException e) {
-//            // TODO Auto-generated catch block
-//            return "";
-//        }
-//        weekdayFormat = weekdayF.format(d);
-//        return weekdayFormat;
-//    }
 
     public int getTime() {
         return 0;
     }
-//        time = exifInterface.getAttribute(ExifInterface.TAG_DATETIME_ORIGINAL);
-//        SimpleDateFormat timeF = new SimpleDateFormat("HH");
-//        try {
-//            d = timeF.parse(time);
-//        } catch (ParseException e) {
-//            // TODO Auto-generated catch block
-//            return 0;
-//        }
-//        timeFormat = timeF.format(d);
-//        return dayTime;
-
-//    }
-
-
-
 
     //TODO update these with the appropriate calculations
     //methods to determine what weights to add onto the photos
@@ -108,35 +59,11 @@ public class Photo {
     //TODO replace instances of "false" with calculations
     //method to return a weight for the image based on how recently the photo was taken
     public int recentlyTakenWeight() {
-        if(within_a_week()) {
-            return 200;
-        }
-        if(within_a_month()) {
-            return 100;
-        }
-        if(within_a_year()) {
-            return 50;
-        }
-        else return 0;
+        return 0;
     }
 
     //method to calcualte the overall weight of the photo
     public void calcWeight(){
-        //TODO check whether the time of day is within an hour of the time the pic was taken
-        weight = 300;
 
-        if(is_dayTime()) {
-            weight += 100;
-        }
-        if(is_weekday()) {
-            weight += 100;
-        }
-        if(is_location()) {
-            weight += 100;
-        }
-        if(karma) {
-            weight += 200;
-        }
-        weight += recentlyTakenWeight();
     }
 }
