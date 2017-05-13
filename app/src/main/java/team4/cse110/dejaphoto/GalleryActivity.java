2a235fc45f3ad514e5d792712bf3c53588dde5c1
Bofan <bfchen1998@gmail.com>
2017-05-13 02:59:11 -0700

    T4-25: Started adding onClickListener's to gallery photos.

--- app/src/main/java/team4/cse110/dejaphoto/GalleryActivity.java
+++ app/src/main/java/team4/cse110/dejaphoto/GalleryActivity.java
@@ -1,10 +1,17 @@
 package team4.cse110.dejaphoto;
 
 import android.os.Bundle;
+import java.io.File;
+
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -13,6 +20,11 @@ public class GalleryActivity extends AppCompatActivity {
     private static final int GRID_SPAN = 3;
 
     private ArrayList<Photo> photos;
+
+    /** Create a new directory to store selected folders. */
+    static final String dirName = "DejaPhoto";
+    static final File imageRoot = new File(Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_PICTURES), dirName);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +42,14 @@ public class GalleryActivity extends AppCompatActivity {
         rvPhotos.setAdapter(adapter);
         rvPhotos.setLayoutManager(new GridLayoutManager(this, GRID_SPAN));
 
+        /** Create onClickListener()'s for each photo in the GridView */
+        OnItemClickListener pictureSelect
+            = new OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String prompt = (String)parent.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), prompt, Toast.LENGTH_LONG).show();
+            }
+        };
     }
 }
