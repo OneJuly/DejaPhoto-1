efe9f9d97b27f0b1d5e5c26327e50d86b7c59ff9
Jean-Luc Thumm <jeanlucthumm@gmail.com>
2017-05-14 18:45:55 -0700

    Implement next()

--- app/src/main/java/team4/cse110/dejaphoto/DejaAlgorithm.java
+++ app/src/main/java/team4/cse110/dejaphoto/DejaAlgorithm.java
@@ -3,7 +3,8 @@ package team4.cse110.dejaphoto;
 import android.content.Context;
 import android.graphics.Bitmap;
 
-import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
 
 public class DejaAlgorithm implements Algorithm {
 
@@ -11,10 +12,9 @@ public class DejaAlgorithm implements Algorithm {
     private PhotoDB db;
 
     // Database dependent variables
-    private ArrayList<Photo> album;
-    private ArrayList<Photo> cache;
-    private int cachePos;
-
+    private List<Photo> album;
+    private List<Photo> cache;
+    private int cachePos;   // last returned by prev() or next() or release(). -1 if cache empty
 
     public DejaAlgorithm(Context context) {
         this.context = context;
@@ -24,7 +24,35 @@ public class DejaAlgorithm implements Algorithm {
 
     @Override
     public Bitmap next() {
-        return null;
+        if (album.isEmpty()) return null;
+
+        // Compute weight for all images
+        for (Photo photo : album) {
+            setWeight(photo);
+        }
+
+        // Pick an image randomly while factoring in weights
+        double totalWeight = 0.0;
+        for (Photo photo : album) {
+            totalWeight += photo.getWeight();
+        }
+
+        Photo chosenPhoto = null;
+        double random = totalWeight * Math.random();
+        for (Photo photo : album) {
+            random -= photo.getWeight();
+            if (random <= 0) {
+                chosenPhoto = photo;
+                break;
+            }
+        }
+        assert chosenPhoto != null;
+
+        // Update cache
+        addToCache(chosenPhoto);
+        save();
+
+        return chosenPhoto.getBitmap();
     }
 
     @Override
@@ -55,5 +83,36 @@ public class DejaAlgorithm implements Algorithm {
     @Override
     public void load() {
 
+    }
+
+    /**
+     * @return photo currently set as the wallpaper
+     */
+    private Photo getCurrentPhoto() {
+        return cache.get(cachePos);
+    }
+
+    /**
+     * Computes the weight for every photo in the album
+     */
+    private void setWeight(Photo photo) {
+        // TODO add time into the calculation. Android has System.currentTimeMillis()
+
+    }
+
+    private void addToCache(Photo photo) {
+        // Check if we need to remove everything after pointer
+        if (cachePos != cache.size() - 1) {
+            ListIterator<Photo> itr = cache.listIterator(cachePos);
+            itr.remove(); // remove self
+            while (itr.hasNext()) {
+                itr.next();
+                itr.remove();
+            }
+        }
+
+        // Add photo and reset pointer
+        cache.add(photo);
+        cachePos = cache.size() - 1;
     }
 }
