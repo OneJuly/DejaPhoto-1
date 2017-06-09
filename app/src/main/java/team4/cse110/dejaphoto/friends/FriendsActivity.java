54bba7fd658d48f0c776c88a48e93a1d6ce1a088
Joel Yun <j2yun@ucsd.edu>
2017-06-09 02:53:21 -0700

    t4-71 currently gets list of all users and updates when the user list changes

--- app/src/main/java/team4/cse110/dejaphoto/friends/FriendsActivity.java
+++ app/src/main/java/team4/cse110/dejaphoto/friends/FriendsActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -24,6 +25,8 @@ import team4.cse110.dejaphoto.R;
 public class FriendsActivity extends BaseActivity {
     /* private member variables */
     private DatabaseReference usersdb;
+    private DatabaseReference usersID;
+    private DatabaseReference friendsID;
     private ListView friendsNameView;
     private List<String> friendsNames = new ArrayList<>();
 
@@ -34,22 +37,32 @@ public class FriendsActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_friends);
+
+        //to store the names
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        usersdb = FirebaseDatabase.getInstance().getReference().child("Friends");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+
+        usersdb = FirebaseDatabase.getInstance().getReference();
+        //usersdb.getKey();
+        //usersdb = FirebaseDatabase.getInstance().getReference().child("A87fcgB4XOdlla7IiiN4pMC4FUy1").child("friendsList");
         friendsNameView = (ListView)findViewById(R.id.friendList);
+
         final ArrayAdapter<String> arrayAdapter =
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friendsNames);
         friendsNameView.setAdapter(arrayAdapter);
 
+
         usersdb.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                friendsNames.clear();
                 for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
-                    String friend = postSnapshot.getValue(String.class);
+                    String friend = postSnapshot.getKey();
                     friendsNames.add(friend);
                     arrayAdapter.notifyDataSetChanged();
                 }
@@ -59,6 +72,79 @@ public class FriendsActivity extends BaseActivity {
             public void onCancelled(DatabaseError databaseError) {
             }
         });
+
+        /*
+        usersdb.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
+                    String friend = postSnapshot.getKey();
+                    friendsNames.add(friend);
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                //String value = dataSnapshot.getValue(String.class);
+                //String key = dataSnapshot.getKey();
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+        */
+
+/*
+        friendsNameView = (ListView)findViewById(R.id.friendList);
+        final ArrayAdapter<String> arrayAdapter =
+                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friendsNames);
+        friendsNameView.setAdapter(arrayAdapter);
+
+        //loop through users
+        usersID = FirebaseDatabase.getInstance().getReference();
+        usersID.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot1) {
+                for (DataSnapshot postSnapshot1: dataSnapshot1.getChildren()) {
+
+                    //loop through user's friends
+                    friendsID = FirebaseDatabase.getInstance().getReference();
+                    friendsID.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot2) {
+                            for (DataSnapshot postSnapshot2 : dataSnapshot2.getChildren()) {
+                                String friend = postSnapshot2.getValue(String.class);
+                                friendsNames.add(friend);
+                                arrayAdapter.notifyDataSetChanged();
+                            }
+                        }
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                            //...
+                        }
+                    });
+
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                //...
+            }
+        });
+*/
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
