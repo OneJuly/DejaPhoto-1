68f599b1f307f9f372699121847a2415ce5f9226
OneJuly <joelyun@gmail.com>
2017-06-09 18:49:15 -0700

    JUnit skeleton to test PhotoDatabase / interface?

---
+++ app/src/test/java/team4/cse110/dejaphoto/PhotoDatabaseJUnitTest.java
@@ -0,0 +1,129 @@
+package team4.cse110.dejaphoto;
+
+import android.test.mock.MockContext;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import team4.cse110.dejaphoto.database.PhotoDatabase;
+import team4.cse110.dejaphoto.photo.Photo;
+import team4.cse110.dejaphoto.utilities.DejaAlgorithm;
+
+import static junit.framework.Assert.assertEquals;
+
+/*
+ * Created by cs110sdn on 6/9/2017.
+ */
+
+public class PhotoDatabaseJUnitTest {
+    @Before
+    public void setup() {
+        /* CREATE NEEDED MOCK OBJECTS */
+        MockContext context = new MockContext();
+        //PhotoDatabase db = new PhotoDatabase() {
+        Photo photo = new Photo(context, "2_Mt_Everest.jpg", true);
+    }
+
+    /**
+     * This method tests the addPhoto method.
+     */
+    @Test
+     public void testaddPhoto() {
+
+     assertEquals(0, 0);
+
+      /* TODO assertEquals(    ); */
+    }
+
+    /**
+     * This method tests the deletePhoto method.
+     */
+    @Test
+    public void testdeletePhoto() {
+
+        assertEquals(0, 0);
+
+        /*  TODO assertEquals(    ); */
+    }
+
+    /**
+     * This method tests the setSharePhotos method.
+     */
+    @Test
+    public void testsetSharePhotos() {
+
+        assertEquals(0, 0);
+
+        /* TODO assertEquals(   ); */
+    }
+    /**
+     * This method tests the getPhotoList method.
+     */
+    @Test
+    public void testgetPhotoList() {
+
+        assertEquals(0, 0);
+
+      /* TODO assertEquals(    ); */
+    }
+
+    /**
+     * This method tests the fetchBitmap method.
+     */
+    @Test
+    public void testfetchBitmap() {
+
+        assertEquals(0, 0);
+
+        /*  TODO assertEquals(    ); */
+    }
+
+    /**
+     * This method tests the storePreviousList method.
+     */
+    @Test
+    public void teststorePreviousList() {
+
+        assertEquals(0, 0);
+
+      /* TODO assertEquals(    ); */
+    }
+
+    /**
+     * This method tests the getPreviousList method.
+     */
+    @Test
+    public void testgetPreviousList() {
+
+        assertEquals(0, 0);
+
+        /*  TODO assertEquals(    ); */
+    }
+
+    /**
+     * This method tests the storePreviousList method.
+     */
+    @Test
+    public void testStorePreviousIndex() {
+
+        assertEquals(0, 0);
+
+      /* TODO assertEquals(    ); */
+    }
+
+    /**
+     * This method tests the getPreviousIndex method.
+     */
+    @Test
+    public void testgetPreviousIndex() {
+
+        assertEquals(0, 0);
+
+        /*  TODO assertEquals(    ); */
+    }
+
+}
+
+
